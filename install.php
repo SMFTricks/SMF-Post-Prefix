b3d82c78786675321d9a1d99e091794e74d0c107
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-13 00:12:27 -0600

    add boards and groups columns
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- install.php
+++ install.php
@@ -5,7 +5,7 @@
  * @version 4.0
  * @author Diego Andrés <diegoandres_cortes@outlook.com>
  * @copyright Copyright (c) 2022, SMF Tricks
- * @license MIT
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
  */
 
 	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
@@ -71,9 +71,9 @@
 					'not_null' => true,
 				],
 				[
-					'name' => 'icon_url',
+					'name' => 'icon_class',
 					'type' => 'varchar',
-					'size' => 500,
+					'size' => 50,
 					'not_null' => false,
 					'default' => null,
 				],
@@ -86,6 +86,66 @@
 				[
 					'type' => 'index',
 					'columns' => ['name'],
+				],
+			],
+			'if_exists' => 'ignore',
+			'error' => 'fatal',
+			'parameters' => [],
+		];
+
+		// Prefixes boards
+		$tables[] = [
+			'table_name' => '{db_prefix}postprefixes_boards',
+			'columns' => [
+				[
+					'name' => 'id_prefix',
+					'type' => 'smallint',
+					'size' => 5,
+					'unsigned' => true,
+					'not_null' => true,
+				],
+				[
+					'name' => 'id_board',
+					'type' => 'smallint',
+					'size' => 5,
+					'unsigned' => true,
+					'not_null' => true,
+				],
+			],
+			'indexes' => [
+				[
+					'type' => 'primary',
+					'columns' => ['id_prefix', 'id_board'],
+				],
+			],
+			'if_exists' => 'ignore',
+			'error' => 'fatal',
+			'parameters' => [],
+		];
+
+		// Prefixes groups
+		$tables[] = [
+			'table_name' => '{db_prefix}postprefixes_groups',
+			'columns' => [
+				[
+					'name' => 'id_prefix',
+					'type' => 'smallint',
+					'size' => 5,
+					'unsigned' => true,
+					'not_null' => true,
+				],
+				[
+					'name' => 'id_group',
+					'type' => 'smallint',
+					'size' => 5,
+					'unsigned' => false,
+					'not_null' => true,
+				],
+			],
+			'indexes' => [
+				[
+					'type' => 'primary',
+					'columns' => ['id_prefix', 'id_group'],
 				],
 			],
 			'if_exists' => 'ignore',
