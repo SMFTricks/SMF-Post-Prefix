5dd0b5474c9d4ffe0c0a347820acfad4c292d2a6
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-16 17:21:26 -0600

    handle logic for upgrade
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- install.php
+++ install.php
@@ -182,4 +182,205 @@
 				'unsigned' => true,
 			]
 		);
+
+		// Do some additional changes
+		$prefix_columns = $smcFunc['db_list_columns']('{db_prefix}postprefixes', true);
+
+		if (!empty($prefix_columns))
+		{
+			// Add the keys
+			$smcFunc['db_add_index'](
+				'{db_prefix}postprefixes',
+				[
+					'type' => 'index',
+					'columns' => ['name', 'status'],
+				],
+				[],
+				'ignore',
+			);
+
+			// ID
+			if (isset($prefix_columns['id']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'id',
+					[
+						'type' => 'smallint',
+						'size' => 5,
+						'auto' => true,
+						'not_null' => true,
+						'unsigned' => true,
+					]
+				);
+			}
+			// Name
+			if (isset($prefix_columns['name']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'name',
+					[
+						'type' => 'varchar',
+						'size' => 25,
+						'not_null' => false,
+						'default' => null,
+					]
+				);
+			}
+			// Status
+			if (isset($prefix_columns['status']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'status',
+					[
+						'type' => 'tinyint',
+						'size' => 1,
+						'unsigned' => true,
+					]
+				);
+			}
+			// Color
+			if (isset($prefix_columns['color']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'color',
+					[
+						'type' => 'varchar',
+						'size' => 255,
+						'not_null' => false,
+						'default' => null,
+					]
+				);
+			}
+			//BgColor
+			if (isset($prefix_columns['bgcolor']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'bgcolor',
+					[
+						'type' => 'tinyint',
+						'size' => 1,
+						'unsigned' => true,
+						'not_null' => true,
+					]
+				);
+			}
+			// Invert Color
+			if (isset($prefix_columns['invert_color']))
+			{
+				$smcFunc['db_change_column'](
+					'{db_prefix}postprefixes',
+					'invert_color',
+					[
+						'type' => 'tinyint',
+						'size' => 1,
+						'unsigned' => true,
+						'not_null' => true,
+					]
+				);
+			}
+
+			// Icon Class not added?
+			if (!isset($prefix_columns['icon_class']))
+			{
+				$smcFunc['db_add_column'](
+					'{db_prefix}postprefixes',
+					[
+						'name' => 'icon_class',
+						'type' => 'varchar',
+						'size' => 50,
+						'not_null' => false,
+						'default' => null,
+					],
+					[],
+					'ignore',
+				);
+			}
+
+			// Remove icon_url
+			if (isset($prefix_columns['icon_url']))
+			{
+				$smcFunc['db_remove_column'](
+					'{db_prefix}postprefixes',
+					'icon_url'
+				);
+			}
+
+			// Get the boards and groups
+			if (isset($prefix_columns['boards']) && isset($prefix_columns['groups']))
+			{
+				$request = $smcFunc['db_query']('', '
+					SELECT id, boards, groups
+					FROM {db_prefix}postprefixes',
+					[],
+				);
+				$prefix_groups = [];
+				$prefix_boards = [];
+				while ($row = $smcFunc['db_fetch_assoc']($request))
+				{
+					// Check for boards?
+					if (isset($row['boards']) )
+					{
+						foreach (explode(',', $row['boards']) as $board)
+						{
+							$prefix_boards[] = [
+								'id_prefix' => $row['id'],
+								'id_board' => $board,
+							];
+						}
+					}
+
+					// Check for groups
+					if (isset($row['groups']))
+					{
+						foreach (explode(',', $row['groups']) as $group)
+						{
+							$prefix_groups[] = [
+								'id_prefix' => $row['id'],
+								'id_group' => $group,
+							];
+						}
+					}
+
+				}
+				$smcFunc['db_free_result']($request);
+
+				// Drop boards
+				$smcFunc['db_remove_column']('{db_prefix}postprefixes', 'boards');
+				// Drop groups
+				$smcFunc['db_remove_column']('{db_prefix}postprefixes', 'groups');
+			}
+		}
+
+		// Add the boards if any
+		if (!empty($prefix_boards))
+		{
+			$smcFunc['db_insert']('ignore',
+				'{db_prefix}postprefixes_boards',
+				[
+					'id_prefix' => 'int',
+					'id_board' => 'int'
+				],
+				$prefix_boards,
+				['id_prefix', 'id_board']
+			);
+		}
+
+		// Add the groups if any
+		if (!empty($prefix_groups))
+		{
+			$smcFunc['db_insert']('ignore',
+				'{db_prefix}postprefixes_groups',
+				[
+					'id_prefix' => 'int',
+					'id_group' => 'int'
+				],
+				$prefix_groups,
+				['id_prefix', 'id_group']
+			);
+		}
 	}
