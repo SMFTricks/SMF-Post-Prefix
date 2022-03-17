d2fc6f97bb2e88a8bd011471952a2336db064189
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-16 19:56:19 -0600

    New translations .english.php (spanish_latin)

---
+++ Themes/default/languages/PostPrefix/.spanish_latin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @package SMF Post Prefix
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+// Admin Tabs
+$txt['PostPrefix_main'] = 'Prefijos';
+$txt['PostPrefix_tab_general'] = '';
+$txt['PostPrefix_tab_general_desc'] = 'Información general sobre el mod de prefijos.';
+$txt['PostPrefix_tab_prefixes'] = 'Prefijos';
+$txt['PostPrefix_tab_prefixes_desc'] = 'La lista de prefijos disponibles';
+$txt['PostPrefix_tab_prefixes_add'] = 'Agregar prefijo';
+$txt['PostPrefix_tab_prefixes_add_desc'] = 'Añadir un prefijo nuevo, puedes ajustar el nombre, color y seleccionar los foros y grupos permitidos.';
+$txt['PostPrefix_tab_prefixes_edit_desc'] = 'Editor el prefijo actual, puedes ajustar el nombre, color y seleccionar los foros y grupos permitidos.';
+$txt['PostPrefix_tab_options'] = 'Ajustes';
+$txt['PostPrefix_tab_options_desc'] = 'Ajustes para el mod de prefijos.';
+$txt['PostPrefix_tab_prefixes_edit'] = 'Editar Prefijo';
+$txt['PostPrefix_tab_require'] = 'Requerir En';
+$txt['PostPrefix_tab_require_desc'] = 'Aquí puedes seleccionar en que foros el prefijo es requerido/obligatorio.';
+$txt['PostPrefix_tab_permissions'] = 'Permisos';
+
+// Prefixes main
+$txt['PostPrefix_no_prefixes'] = 'No se han agregado prefijos todavía!';
+$txt['PostPrefix_prefix_name'] = 'Nombre';
+$txt['PostPrefix_prefix_modify'] = 'Modificar';
+$txt['PostPrefix_prefix_status'] = 'Estado';
+$txt['PostPrefix_prefix_groups'] = 'Grupos';
+$txt['PostPrefix_prefix_boards'] = 'Foros';
+$txt['PostPrefix_prefix_id'] = '';
+$txt['prefix'] = 'Prefijo';
+
+// Add/Edit
+$txt['PostPrefix_prefix_enable'] = '¿Activar prefijo?';
+$txt['PostPrefix_prefix_color'] = '¿Utilizar color?';
+$txt['PostPrefix_add_prefix'] = 'Agregar prefijo';
+$txt['PostPrefix_save_prefix'] = 'Guardar prefijo';
+$txt['PostPrefix_select_visible_groups'] = 'Mostrar grupos';
+$txt['PostPrefix_prefix_groups_desc'] = 'Grupos permitidos para utilizar el prefijo';
+$txt['PostPrefix_select_visible_boards'] = 'Mostrar foros';
+$txt['PostPrefix_prefix_boards_desc'] = 'Foros en los que el prefijo puede utilizarse';
+$txt['PostPrefix_use_bgcolor'] = '¿Utilizar color como color de fondo?';
+$txt['PostPrefix_invert_color'] = 'Invertir color';
+$txt['PostPrefix_invert_color_desc'] = 'POr defecto con color de fondo el texto es blanco, invertido es negro.';
+$txt['PostPrefix_use_iconClass'] = '';
+$txt['PostPrefix_icon_class'] = '';
+$txt['PostPrefix_prefix_added'] = 'El prefijo fue agregado con éxito.';
+$txt['PostPrefix_prefix_updated'] = 'El prefijo fue actualizado correctamente.';
+$txt['PostPrefix_prefix_delete_sure'] = '¿Estás seguro de que quieres eliminar los prefijos seleccionados?';
+$txt['PostPrefix_prefix_deleted'] = 'Los prefijos fueron eliminados con éxito.';
+
+// Settings
+$txt['PostPrefix_enable_filter'] = 'Filtrar Prefijos';
+$txt['PostPrefix_enable_filter_desc'] = 'Activa esta opción para mostrar una caja para filtrar temas por prefijo.';
+$txt['PostPrefix_filter_boards'] = 'Foros en los que se mostrará el filtro';
+$txt['PostPrefix_filter_boards_desc'] = 'Selecciona los foros en los que deseas mostrar la caja.';
+$txt['PostPrefix_select_order'] = 'Ordenar por';
+$txt['PostPrefix_select_order_desc'] = 'Selecciona el orden por defecto de los prefijos en cualquier sección.';
+$txt['PostPrefix_prefix_boards_require'] = 'Requerir prefijos';
+$txt['PostPrefix_prefix_boards_require_desc'] = 'foros en los que es necesarioo agregar un prefijo al crrear o editar un tema.';
+$txt['PostPrefix_post_selecttype'] = '';
+$txt['PostPrefix_post_selecttype_desc1'] = '';
+$txt['PostPrefix_post_selecttype_desc2'] = '';
+$txt['PostPrefix_post_selecttype_select'] = '';
+$txt['PostPrefix_post_selecttype_radio'] = '';
+$txt['PostPrefix_prefix_linktree'] = '';
+$txt['PostPrefix_prefix_linktree_desc'] = '';
+$txt['PostPrefix_prefix_boardindex'] = '';
+$txt['PostPrefix_prefix_boardindex_desc'] = '';
+$txt['PostPrefix_prefix_all_msgs'] = '';
+$txt['PostPrefix_prefix_all_msgs_desc'] = '';
+$txt['PostPrefix_prefix_recent_page'] = '';
+$txt['PostPrefix_prefix_recent_page_desc'] = '';
+
+// Error
+$txt['PostPrefix_error_noprefix'] = 'Debes especificar un nombre para el prefijo';
+$txt['PostPrefix_error_unable_tofind'] = 'No se encontró ningún prefijo';
+$txt['error_no_prefix'] = 'No se seleccionó ningún prefijo.';
+$txt['PostPrefix_empty_groups'] = 'No hay grupos para este prefijo.';
+$txt['PostPrefix_empty_boards'] = 'No hay foros para este prefijo.';
+
+// Permissions
+$txt['PostPrefix_permissions'] = '';
+$txt['permissionname_postprefix_manage'] = 'Administrar Prefijos';
+$txt['permissionhelp_postprefix_manage'] = 'Si el usuario puede administrar los prefijos.';
+$txt['cannot_postprefix_manage'] = 'No tienes permiso de administrar los prefijos.';
+$txt['permissionname_postprefix_set'] = 'Utilizar Prefjos';
+$txt['permissionhelp_postprefix_set'] = 'Si el usuario puede utilizar prefijos.';
+$txt['cannot_postprefix_set'] = '';
+
+// Post
+$txt['PostPrefix_select_prefix'] = 'Seleccionar Prefijo';
+$txt['PostPrefix_prefix'] = 'Prefijo';
+$txt['PostPrefix_prefix_none'] = '[Sin Prefijo]';
+
+// Filter by prefix
+$txt['PostPrefix_filter'] = 'Filtrar por prefijo';
+$txt['PostPrefix_filter_noprefix'] = 'Sin prefijo';
+$txt['PostPrefix_filter_all'] = 'Mostrar todos los temas';
