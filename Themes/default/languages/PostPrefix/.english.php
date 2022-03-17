0b94f6a7881e9c54d23643eabb6fa639be20bb13
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-16 19:42:19 -0600

    test
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/languages/PostPrefix/.english.php
+++ Themes/default/languages/PostPrefix/.english.php
@@ -101,3 +101,4 @@ $txt['PostPrefix_prefix_none'] = '[No Prefix]';
 $txt['PostPrefix_filter'] = 'Filter by prefix';
 $txt['PostPrefix_filter_noprefix'] = 'No prefix';
 $txt['PostPrefix_filter_all'] = 'Show all topics';
+$txt['PostPrefix_test_filter'] = 'Test';
